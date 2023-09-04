752d0c9c60aea6a561a143f27d66d2c2519ad47d
alina <akupriianova99@gmail.com>
2023-09-04 13:56:21 +0300

    fix linter errors in Messages.jsx

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,15 +3,17 @@ const Messages = (props) => {
 
   return (
     <div className="px-5">
-      {currentMessages.map((message, index) => <div key={index}>
-        <div className="text-break mb-2">
-          <b>{message.username}</b>
-          : {message.body}
+      {currentMessages.map((message, index) => (
+        <div key={index}>
+          <div className="text-break mb-2">
+            <b>{message.username}</b>
+            :
+            {message.body}
+          </div>
         </div>
-      </div>)}
+      ))}
     </div>
-  )
-
-}
+  );
+};
 
 export default Messages;
